339ef41d9eadbaf65c82dfa525127b181f1c37f0
Jesus Zuñiga <jesuszt@hotmail.com>
2023-04-20 12:41:54 -0600

    Se inicia construcción de la entidad Persona

---
+++ src/main/java/com/zutjmx/batch/entities/Persona.java
@@ -0,0 +1,12 @@
+package com.zutjmx.batch.entities;
+
+import java.util.Date;
+
+public class Persona {
+    private Long id;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private String email;
+    private Date fechaNacimiento;
+}
