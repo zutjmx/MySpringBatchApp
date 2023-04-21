da439096d8579e45e4be6eb21ee95ca133a1c126
Jesus Zuñiga <jesuszt@hotmail.com>
2023-04-21 11:14:16 -0600

    Modificaciones 21/04/2023 11:14

--- src/main/java/com/zutjmx/batch/config/BatchConfiguration.java
+++ src/main/java/com/zutjmx/batch/config/BatchConfiguration.java
@@ -4,30 +4,54 @@ import com.zutjmx.batch.steps.ItemDecompressStep;
 import com.zutjmx.batch.steps.ItemProcessStep;
 import com.zutjmx.batch.steps.ItemReaderStep;
 import com.zutjmx.batch.steps.ItemWriterStep;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfiguration {
 
+    @Autowired
+    public JobBuilder jobBuilder;
+
+    @Autowired
+    public StepBuilder stepBuilder;
+
     @Bean
+    @JobScope
+    public ItemDecompressStep itemDecompressStep() {
+        return new ItemDecompressStep();
+    }
+
+    @Bean
+    @JobScope
     public ItemReaderStep itemReaderStep() {
         return new ItemReaderStep();
     }
 
     @Bean
+    @JobScope
     public ItemProcessStep itemProcessStep() {
         return new ItemProcessStep();
     }
 
     @Bean
+    @JobScope
     public ItemWriterStep itemWriterStep() {
         return new ItemWriterStep();
     }
 
     @Bean
-    public ItemDecompressStep itemDecompressStep() {
-        return new ItemDecompressStep();
+    public Step descompressFileStep() {
+        /*return stepBuilderFactory.get("descompressFileStep")
+                .tasklet(itemDescompressStep())
+                .build();*/
+
+        return null;
     }
 
 }
